87550239c949a34e52ed4bbc6cb5650ec9f383e9
Algernon Soriano <alger.soriano@gmail.com>
2022-08-22 00:20:56 +1000

    first_class_function

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+function returnFirstTwoDrivers(array) {
+    let firstTwo = [array[0], array[1]]
+    return firstTwo
+}
+console.log(returnFirstTwoDrivers(drivers))
+
+function returnLastTwoDrivers(array) {
+    let lastTwo = [array[2], array[3]]
+    return lastTwo
+}
+
+console.log(returnLastTwoDrivers(drivers))
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+console.log(selectingDrivers[0])
+
+function createFareMultiplier(x) {
+    return function fareMultiplier() {
+        return 5 * x
+    }
+}
+console.log(createFareMultiplier(2))
+
+let fareDoubler = x => x * 2;
+let fareTripler = y => y * 3;
+
+function selectDifferentDrivers(arrayOfDrivers, whichSet) {
+    return whichSet(arrayOfDrivers)
+}
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers()))
